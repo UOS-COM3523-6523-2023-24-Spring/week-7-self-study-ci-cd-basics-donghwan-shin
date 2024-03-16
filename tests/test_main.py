5a752702dd622f4909b13bee2ca22b7396300dfd
Donghwan Shin <d.shin@sheffield.ac.uk>
2024-03-16 23:04:45 +0000

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 import unittest
 from main import simple_count, complex_function
+from unittest.mock import patch
 
 
 class MyTestCase(unittest.TestCase):
@@ -9,5 +10,11 @@ class MyTestCase(unittest.TestCase):
     def test_simple_function2(self):
         self.assertEqual(5, simple_count('hello'))
 
-    # def test_complex_function(self):
-    #     self.assertEqual('behaviour 1', complex_function())
+    def test_complex_function(self):
+        self.assertEqual('behaviour 1', complex_function())
+
+    @patch('main.random_function')
+    def test_complex_function_with_mock(self, mock_random_function):
+        mock_random_function.return_value = True
+        self.assertEqual('behaviour 1', complex_function())
+        
